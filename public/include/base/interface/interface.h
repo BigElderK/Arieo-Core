15989f5b22a954a9a44e7e3bd1d063a43545593b
BigElderK <BigElderK@divineland.org>
2026-02-25 23:09:27 +0800

    Add createAs, castTo, and destroyAs methods to Interface class; enhance logging for instance creation and destruction

--- public/include/base/interface/interface.h
+++ public/include/base/interface/interface.h
@@ -1,7 +1,9 @@
 #pragma once
 #include "base/delegates/function_delegate.h"
+#include "core/logger/logger.h"
 #include <cstddef>
 #include <type_traits>
+
 
 #define METADATA(x) [[clang::annotate(#x)]]
 
@@ -51,8 +53,48 @@ namespace Arieo::Base
 
         bool operator==(Interface<TInterface> const& other) const { return m_interface == other.m_interface; }
         bool operator!=(Interface<TInterface> const& other) const { return m_interface != other.m_interface; }
-        // TInterface* get() { return m_interface; }
-        // const TInterface* get() const { return m_interface; }
+
+        template<typename TInstance, typename... Args>
+        static Interface<TInterface> createAs(Args&&... args)
+        {
+            const std::size_t hash = std::hash<std::string_view>{}(typeid(TInstance).name());
+            Core::Logger::trace("Creating instance of type {} with hash {}", typeid(TInstance).name(), hash);
+            TInstance* new_instance = new (Arieo::Base::Memory::malloc(sizeof(TInstance) + sizeof(std::size_t))) TInstance(std::forward<Args>(args)...);
+            std::size_t* type_hash_ptr = reinterpret_cast<std::size_t*>(reinterpret_cast<std::byte*>(new_instance) + sizeof(TInstance));
+            *type_hash_ptr = hash;
+
+            return Interface<TInterface>(new_instance);
+        }
+
+        template <typename TInstance>
+        TInstance* castTo()
+        {
+            const std::size_t hash = std::hash<std::string_view>{}(typeid(TInstance).name());
+            std::size_t* type_hash_ptr = reinterpret_cast<std::size_t*>(reinterpret_cast<std::byte*>(static_cast<TInstance*>(m_interface)) + sizeof(TInstance));
+            // if(*type_hash_ptr != hash)            
+            // {
+            //     Core::Logger::fatal("Type hash mismatch when casting instance. Expected: {}, Actual: {}", hash, *type_hash_ptr);
+            //     return nullptr;
+            // }
+            return static_cast<TInstance*>(m_interface);
+        }
+
+        template<typename TInstance>
+        void destroyAs()
+        {
+            const std::size_t hash = std::hash<std::string_view>{}(typeid(TInstance).name());
+            std::size_t* type_hash_ptr = reinterpret_cast<std::size_t*>(reinterpret_cast<std::byte*>(m_interface) + sizeof(TInstance));
+            
+            // if(*type_hash_ptr != hash)
+            // {
+            //     Core::Logger::fatal("Type hash mismatch when destroying instance. Expected: {}, Actual: {}", hash, *type_hash_ptr);
+            //     return;
+            // }
+
+            reinterpret_cast<TInstance*>(m_interface)->~TInstance();
+            Arieo::Base::Memory::free(m_interface);
+            m_interface = nullptr;
+        }
     };
 
     template<typename T>
@@ -60,17 +102,5 @@ namespace Arieo::Base
     {
     };
 
-    template <typename TInstance, typename TInterfaceLike>
-    inline TInstance* castInterfaceToInstance(TInterfaceLike interface)
-    {
-        using InterfaceArg = std::remove_cv_t<std::remove_reference_t<TInterfaceLike>>;
-        if constexpr (std::is_pointer_v<InterfaceArg>)
-        {
-            return static_cast<TInstance*>(interface);
-        }
-        else
-        {
-            return static_cast<TInstance*>(interface.operator->());
-        }
-    }
+
 }
